d2c98eb8b724d0d229e12720937f7bb381b8bbc2
Hieu <vanphuhieu811@gmail.com>
2026-04-16 18:42:00 +0700

    update user and role

---
+++ Presenters/RolePresenter.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Oracle.ManagedDataAccess.Client;
+using ATBM_Project.Data;
+using ATBM_Project.Models;
+
+namespace ATBM_Project.Presenters
+{
+    public class RolePresenter
+    {
+        public List<RoleAccount> GetRoles()
+        {
+            List<RoleAccount> list = new List<RoleAccount>();
+            using (OracleConnection conn = DBConfig.GetConnection())
+            {
+                conn.Open();
+                string sql = "SELECT ROLE, ROLE_ID FROM DBA_ROLES";
+                using (OracleCommand cmd = new OracleCommand(sql, conn))
+                using (OracleDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new RoleAccount
+                        {
+                            RoleName = reader["ROLE"].ToString(),
+                            RoleId = reader["ROLE_ID"].ToString()
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+        public void CreateRole(string roleName)
+        {
+            using (OracleConnection conn = DBConfig.GetConnection())
+            {
+                conn.Open();
+                string sql = $"CREATE ROLE {roleName}";
+                using (OracleCommand cmd = new OracleCommand(sql, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DropRole(string roleName)
+        {
+            using (OracleConnection conn = DBConfig.GetConnection())
+            {
+                conn.Open();
+                string sql = $"DROP ROLE {roleName}";
+                using (OracleCommand cmd = new OracleCommand(sql, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
